5ae009c408caef3a51ffcd2be04784a6fd94f4bc
HaydarJohn <haydarcan_2003@hotmail.com>
2022-11-18 19:49:46 +0300

    Maked the game half baked because i will format

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
-#include<iostream>
+#include "pch.h"
+
+void FizznBuzz();
 
 int main(){
-	std::cout<<"It Works!"<<std::endl;
+	
+	std::cout<<"Hi welcome to fizz&buzz game!"<<std::endl;
+	
+	std::cout<<"START \nMODES \nQUIT"<<std::endl;
+
+	string userInput;
+
+	while(true)
+	{
+		getline(cin , userInput);
+
+		tolower(userInput);
+
+		if("start" == userInput)
+		{
+			FizznBuzz();
+		}
+		else if("mode" == userInput)
+		{
+			std::cout<<"Welcome modes menu! \nUnfortunateley for now modes are under construction."<<std::endl;
+			std::cout<<"RACE AGAINST TIME \n"
+			continue;
+		}
+		else if ("quit" == userInput || "quıt" == userInput)
+		{
+			return 0;
+		}
+		
+	}
 }
